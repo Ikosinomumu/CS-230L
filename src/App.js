1125ee2933d606dafc40e98faa0172847a2c7a7a
Ikosinomumu <ioo00001@mix.wvu.edu>
2024-02-25 05:05:00 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div> <h1>CS 230L</h1>
+    <h2>Section 002</h2>
+    <p>WVU ID: 800433217</p>
+    <p>Hi I am Ikechukwu Obiadimma Osinomumu</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
