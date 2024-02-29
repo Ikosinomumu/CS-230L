4ea542670d5a2a3b7f64e21e4cb34636b529db0c
Ikosinomumu <ioo00001@mix.wvu.edu>
2024-02-29 10:00:05 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>CS 230L</h1> 
-      <h2>Section 002</h2>
+      <h2>Section - 002</h2>
       <p>WVU ID: 800433217</p>
       <p>"Hi I am Ikechukwu Obiadimma Osinomumu!</p>
     </div>
