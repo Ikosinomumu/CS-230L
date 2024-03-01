548014589cd85390a417013bccf37a514e28d106
Ikosinomumu <ioo00001@mix.wvu.edu>
2024-03-01 13:46:48 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './Card';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1> 
       <h2>Section - 002</h2>
       <p>WVU ID: 800433217</p>
-      <p>"Hi I am Ikechukwu Obiadimma Osinomumu!</p>
+      <p>"Hi I am Ikechukwu Obiadimma Osinomumu!</p>      
+      <Card />      
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Card() {
+  return (
+
+<div>
+
+    <div class="card text-bg-primary mb-3" style={{maxWidth:400, display:'inline-block', margin:20}}>
+      <div class="card-header">Card01</div>
+      <div class="card-body">
+        <h5 class="card-title">Primary card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    <div class="card text-bg-secondary mb-3" style={{maxWidth:400, display:'inline-block', margin:20}}>
+      <div class="card-header">Card02</div>
+      <div class="card-body">
+        <h5 class="card-title">Secondary card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    <div class="card text-bg-success mb-3" style={{maxWidth:400, display:'inline-block', margin:20}}>
+      <div class="card-header">Card03</div>
+      <div class="card-body">
+        <h5 class="card-title">Success card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+
+</div>
+
+  );
+}
+
+export default Card;
